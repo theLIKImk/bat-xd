9306683bc6aba82707cd0939bbc20aa18b56286d
LIKImk <103368018+theLIKImk@users.noreply.github.com>
2025-09-05 19:23:27 +0800

    Update BAT-xd.bat

--- bin/BAT-xd.bat
+++ bin/BAT-xd.bat
@@ -2,7 +2,7 @@
 CHCP 65001
 setlocal EnableDelayedExpansion
 set PATH=%PATH%;%~dp0
-set BAT_XD_VER=0.1.0
+set BAT_XD_VER=0.1.2
 set BAT_XD_NOW_READ=0
 set BAT_XD_READ_LINE=30
 set BAT_XD_READ_PAGE_LINE=3
@@ -32,7 +32,7 @@ call pid
 
 	del /f /s /q "!PIDMD_ROOT!TMP\platelist.txt" >nul
 
-	call pid /start solo nmbxd.bat getForumList
+	call pid /start solo hiderun.cmd nmbxd.bat getForumList
 
 :getForumList-show
 	if not exist "!PIDMD_ROOT!TMP\platelist.txt" goto :getForumList-show
@@ -48,6 +48,12 @@ call pid
 	goto :cf_id_act
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:showf-getNAME
+	call loadcfg "!PIDMD_ROOT!TMP\platein.ini"
+	set plate_id=!%1!
+	call loadcfg "!PIDMD_ROOT!TMP\plate_!plate_id!_!showf_id!_msg.ini"
+exit /b
+
 :showf
 	
 	cls
@@ -55,11 +61,14 @@ call pid
 	set BAT_XD_NOW_READ=0
 	if not defined nmd_page_showf set nmd_page_showf=1
 	set BAT_XD_SHOWF_FILE=showf_id_!showf_id!_page_!nmd_page_showf!_list.txt
-	title !nmd_title! 串串列表        第!nmd_page_showf!页
+	
+	call :showf-getNAME !showf_id!
+	
+	title !nmd_title!  !NAME!板块     第!nmd_page_showf!页
 	call :loading_text
 	
 	del /f /s /q "!PIDMD_ROOT!TMP\!BAT_XD_SHOWF_FILE!" >nul
-	call pid /start solo nmbxd.bat showf !showf_id! !nmd_page_showf!
+	call pid /start solo hiderun.cmd nmbxd.bat showf !showf_id! !nmd_page_showf!
 
 :showf-show
 	REM if not "!errorlevel!"=="0" echo.失败！ & pause & goto :getForumList
@@ -76,7 +85,7 @@ call pid
 	if /i "!user_input:~0,4!"=="page" (
 		if /i "!user_input:~5!"=="+" set /a nmd_page_showf+=1 & goto :showf
 		if /i "!user_input:~5!"=="-" if not "!nmd_page_showf!"=="1" set /a nmd_page_showf-=1 & goto :showf
-		if not "!user_input:~5!" LSS "1" set nmd_page_showf=!user_input:~5! & goto :showf
+		if not "!user_input:~5!" LSS "1" set nmd_page_showf=!user_input:~5!& goto :showf
 	)
 	if /i "!user_input!"=="pu" set /a BAT_XD_NOW_READ=!BAT_XD_NOW_READ! - !BAT_XD_READ_PAGE_LINE! & GOTO :showf-show
 	if /i "!user_input!"=="pd" set /a BAT_XD_NOW_READ=!BAT_XD_NOW_READ! + !BAT_XD_READ_PAGE_LINE! & GOTO :showf-show
@@ -90,11 +99,11 @@ call pid
 	set BAT_XD_THREAD_FILE=thread_!th_id!_page_!nmd_page_thread!_list.txt
 
 	cls
-	title !nmd_title!串 !user_input!        第!nmd_page_thread!页
+	title !nmd_title!  NO.!th_id!    第!nmd_page_thread!页
 	call :loading_text
 	
 	del /f /s /q "!PIDMD_ROOT!TMP\!BAT_XD_THREAD_FILE!" >nul
-	call pid /start solo nmbxd.bat thread !th_id! !nmd_page_thread!
+	call pid /start solo hiderun.cmd nmbxd.bat thread !th_id! !nmd_page_thread!
 	
 :thread-show
 	if not exist "!PIDMD_ROOT!TMP\!BAT_XD_THREAD_FILE!" goto :thread-show
@@ -110,7 +119,7 @@ call pid
 	if /i "!user_input:~0,4!"=="page" (
 		if /i "!user_input:~5!"=="+" set /a nmd_page_thread+=1 & goto :thread
 		if /i "!user_input:~5!"=="-" if not "!nmd_page_thread!"=="1" set /a nmd_page_thread-=1 & goto :thread
-		if not "!user_input:~5!" LSS "1" set nmd_page_thread=!user_input:~5! & goto :thread
+		if not "!user_input:~5!" LSS "1" set nmd_page_thread=!user_input:~5!& goto :thread
 	)
 	if /i "!user_input!"=="pu" set /a BAT_XD_NOW_READ=!BAT_XD_NOW_READ! - !BAT_XD_READ_PAGE_LINE! & GOTO :thread-show
 	if /i "!user_input!"=="pd" set /a BAT_XD_NOW_READ=!BAT_XD_NOW_READ! + !BAT_XD_READ_PAGE_LINE! & GOTO :thread-show
