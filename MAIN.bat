f081cf83ff2908774490dfda7398e4fcc9d840a2
LIKImk <103368018+theLIKImk@users.noreply.github.com>
2025-10-27 08:25:11 +0800

    Update MAIN.bat

--- MAIN.bat
+++ MAIN.bat
@@ -1,5 +1,5 @@
 @echo off
-set Launcher_ver=0.0.2
+set Launcher_ver=0.0.3
 title Launcher %Launcher_ver%
 
 SET BAT_XD_INDIR=%~dp0
@@ -9,7 +9,10 @@ SET BAT_XD_INDIR=%BAT_XD_INDIR:"= %
 SET BAT_XD_INDIR=%BAT_XD_INDIR:(= %
 SET BAT_XD_INDIR=%BAT_XD_INDIR:)= %
 CALL :puth_cut %BAT_XD_INDIR%
-IF defined puth_cut_2 echo 请不要把我放在带有空格/括号/感叹号的路径下面！& pause  & exit /b
+
+IF defined puth_cut_2 echo 请不要把我放在带有空格/括号/感叹号的路径下面！& pause  & exit /b 1001
+
+IF exist "%windir%\system32\winecfg.exe" echo.无法兼容于WINE环境！& pause  & exit /b 1002
 
 cd "%~dp0bin"
 pid /start solo BAT-xd.bat
